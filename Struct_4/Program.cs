571bbfe6e09dabc39f7e57b1cff88bad1faaf979
Ratimir <ra0953029364@gmail.com>
2020-01-20 01:49:55 +0200

    Добавьте файлы проекта.

---
+++ Struct_4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Struct_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BirthdayCalculator();
+            Console.ReadLine();
+        }
+        static void BirthdayCalculator()
+        {
+            Console.WriteLine("Введите дату вашего рождения в формате ГГГГ.ММ.ДД:");
+            string[] birthday = Console.ReadLine().Split('.');
+            try
+            {
+                DateTime date = new DateTime(Convert.ToInt32(birthday[0]), Convert.ToInt32(birthday[1]), Convert.ToInt32(birthday[2]));
+                DateTime ng = new DateTime(DateTime.Now.Year, 12, 31);
+                int ostDay = date.DayOfYear - DateTime.Now.DayOfYear;
+                if (date.Year > DateTime.Now.Year)
+                    throw new Exception();
+                else if (ostDay < 0)
+                {
+                    ostDay = ng.DayOfYear - DateTime.Now.DayOfYear + date.DayOfYear;
+                }
+                Console.WriteLine($"До вашего следующего дня рождения осталось {ostDay} дней!");
+            }
+            catch
+            {
+                Console.WriteLine("Ошибка ввода, попробуйте ещё!");
+                BirthdayCalculator();
+            }
+        }
+    }
+}
